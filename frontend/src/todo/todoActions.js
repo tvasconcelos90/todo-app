7dad7c35bf8b7e951091a34f3b072e1903b41a96
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-02 21:13:21 -0300

    Adding 'markAsPending' and 'remove' actions with redux

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -16,10 +16,32 @@ export const search = () => {
 }
 
 
-export const add = (  description ) => {
+export const add = (description) => {
     return dispatch => {
         axios.post(URL, {description})
             .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsDone = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`,{ ...todo, done: true})
+            .then(resp=> dispatch({type: 'TODO_MARKED_AS_DONE', payload: resp.data}))
+            .then(resp=>dispatch(search()))        
+    }
+}
+
+export const markAsPending = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false})
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const remove = (todo) => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)
             .then(resp => dispatch(search()))
     }
 }
